e5726f9a7680ba913c19839534ab52b9eb2bb6ef
Park-jae-hyeon <pgs1835@naver.com>
2024-02-06 19:09:19 +0900

    foodId 하드코딩 수정

--- fridge-frontend/Fridege-frontend/src/Modal.jsx
+++ fridge-frontend/Fridege-frontend/src/Modal.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ModalForm from './ModalForm'
 
-const Modal = ({onClose, isOpen}) => {
+const Modal = ({onClose, isOpen, foodId}) => {
   return (
     <>
     <div data-cy="modal-backdrop" className='fixed top-0 left-0 w-full h-full backdrop-blur-md z-30' onClick={onClose}>
     </div>
     <div className='fixed z-40 w-1/3 p-8 m-0 transform -translate-x-1/2 -translate-y-1/2 border-none rounded shadow-xl top-1/2 left-1/2 bg-[#C9D7DD]'>
-            <ModalForm onClose={onClose}></ModalForm>
+            <ModalForm onClose={onClose} foodId={foodId}></ModalForm>
         </div>
     </>
   )
